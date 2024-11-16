a4962ec6142ef78cbe37118e6f7180173fb63036
Georgi Panov <77702912+Darkfella91@users.noreply.github.com>
2024-11-16 08:43:35 +0200

    Update entrypoint.sh

--- apps/bazarr/entrypoint.sh
+++ apps/bazarr/entrypoint.sh
@@ -2,7 +2,7 @@
 #shellcheck disable=SC2086
 
 exec \
-    /usr/local/bin/python \
+    /usr/local/bin/python3 \
         /app/bin/bazarr.py \
             --no-update \
             --config /config \
